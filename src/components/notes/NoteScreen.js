f33c8c1fe6beb45cd81ef6ba8ee0e1859aaa2b0b
aac-devs <aac.devs@gmail.com>
2021-02-12 05:12:33 -0300

    Front: Upload files to cloudinary.com done, CRUD finished

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { act } from "react-dom/test-utils";
 import { useDispatch, useSelector } from "react-redux";
-import { activeNote } from "../../actions/notes";
+import { activeNote, startDeleting } from "../../actions/notes";
 import { useForm } from "../../hooks/useForm";
 import { NotesAppBar } from "./NotesAppBar";
 
@@ -9,7 +9,7 @@ export const NoteScreen = () => {
   const dispatch = useDispatch();
   const { active: note } = useSelector((state) => state.notes);
   const [formValues, handleInputChange, reset] = useForm(note);
-  const { title, body } = formValues;
+  const { title, body, id } = formValues;
 
   const activeId = useRef(note.id);
 
@@ -23,6 +23,10 @@ export const NoteScreen = () => {
   useEffect(() => {
     dispatch(activeNote(formValues.id, { ...formValues }));
   }, [formValues, dispatch]);
+
+  const handleDelete = () => {
+    dispatch(startDeleting(id));
+  };
 
   return (
     <div className="notes__main-content">
@@ -46,13 +50,13 @@ export const NoteScreen = () => {
         ></textarea>
         {note.url && (
           <div className="notes__image">
-            <img
-              src="https://www.muycomputer.com/wp-content/uploads/2016/07/portada.jpg"
-              alt="imagen"
-            />
+            <img src={note.url} alt="imagen" />
           </div>
         )}
       </div>
+      <button className="btn btn-danger" onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 };
--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { startSaveNote } from "../../actions/notes";
+import { startSaveNote, startUploading } from "../../actions/notes";
 
 export const NotesAppBar = () => {
   const dispatch = useDispatch();
+  const inputFile = useRef(null);
+
   const { active: note } = useSelector((state) => state.notes);
   const handleSave = () => {
     dispatch(startSaveNote(note));
   };
+  const handlePictureClick = () => {
+    inputFile.current.click();
+  };
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      dispatch(startUploading(file));
+    }
+  };
   return (
     <div className="notes__appbar">
       <span>5 de Febrero de 2021</span>
+      <input
+        ref={inputFile}
+        type="file"
+        name="file"
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
       <div>
-        <button className="btn">Picture</button>
+        <button className="btn" onClick={handlePictureClick}>
+          Picture
+        </button>
         <button className="btn" onClick={handleSave}>
           Save
         </button>
